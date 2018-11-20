96710a4c7f44f49ea9e8974f51b19e841b9b0d63
Karsten Gresch <karstengresch@users.noreply.github.com>
2018-11-20 19:52:42 +0100

    Change greeting

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -12,7 +12,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="Hackers") String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="Folks") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
